68b7e8734ac33b58bf7b424ae076b7cbb1153e49
madisontabor <144239185+madisontabor@users.noreply.github.com>
2024-12-13 00:48:02 -0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,255 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace Assignment_5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.personDataSet = new Assignment_5.PersonDataSet();
+            this.personBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.personTableAdapter = new Assignment_5.PersonDataSetTableAdapters.PersonTableAdapter();
+            this.tableAdapterManager = new Assignment_5.PersonDataSetTableAdapters.TableAdapterManager();
+            this.personBindingNavigator = new System.Windows.Forms.BindingNavigator(this.components);
+            this.bindingNavigatorMoveFirstItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorMovePreviousItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorSeparator = new System.Windows.Forms.ToolStripSeparator();
+            this.bindingNavigatorPositionItem = new System.Windows.Forms.ToolStripTextBox();
+            this.bindingNavigatorCountItem = new System.Windows.Forms.ToolStripLabel();
+            this.bindingNavigatorSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.bindingNavigatorMoveNextItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorMoveLastItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.bindingNavigatorAddNewItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorDeleteItem = new System.Windows.Forms.ToolStripButton();
+            this.personBindingNavigatorSaveItem = new System.Windows.Forms.ToolStripButton();
+            this.personDataGridView = new System.Windows.Forms.DataGridView();
+            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.personDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.personBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.personBindingNavigator)).BeginInit();
+            this.personBindingNavigator.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.personDataGridView)).BeginInit();
+            this.SuspendLayout();
+            //
+            // Adding Name Label
+            //
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.nameLabel.Text = "Name";
+            this.nameLabel.Location = new System.Drawing.Point(100, 300);
+            //
+            // Adding Name TextBox
+            //
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.nameTextBox.Location = new System.Drawing.Point(100, 350);
+            this.nameTextBox.DataBindings.Add("Text", this.personBindingSource, "Name");
+            //
+            // Adding Phone Label
+            //
+            this.phoneLabel = new System.Windows.Forms.Label();
+            this.phoneLabel.Text = "Phone";
+            this.phoneLabel.Location = new System.Drawing.Point(250, 300);
+            //
+            // Adding Phone TextBox
+            //
+            this.phoneTextBox = new System.Windows.Forms.TextBox();
+            this.phoneTextBox.Location = new System.Drawing.Point(250, 350);
+            this.phoneTextBox.DataBindings.Add("Text", this.personBindingSource, "Phone");
+            //
+            // Adding Save Button
+            //
+            this.saveButton = new System.Windows.Forms.Button();
+            this.saveButton.Text = "Save";
+            this.saveButton.Location = new System.Drawing.Point(400, 350);
+            this.saveButton.Click += new System.EventHandler(this.PersonBindingNavigatorSaveItem_Click);
+            //
+            // Adding Add Button
+            //
+            this.addButton = new System.Windows.Forms.Button();
+            this.addButton.Text = "Add";
+            this.addButton.Location = new System.Drawing.Point(550, 350);
+            this.addButton.Click += new System.EventHandler(this.AddButton_Click);
+            //
+            // Adding Delete
+            // 
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.deleteButton.Text = "Delete";
+            this.deleteButton.Location = new System.Drawing.Point(700, 350);
+            this.deleteButton.Click += new System.EventHandler(this.DeleteButton_Click);
+            //
+            // personDataSet
+            // 
+            this.personDataSet.DataSetName = "PersonDataSet";
+            this.personDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // personBindingSource
+            // 
+            this.personBindingSource.DataMember = "Person";
+            this.personBindingSource.DataSource = this.personDataSet;
+            // 
+            // personTableAdapter
+            // 
+            this.personTableAdapter.ClearBeforeFill = true;
+            // 
+            // tableAdapterManager
+            // 
+            this.tableAdapterManager.BackupDataSetBeforeUpdate = false;
+            this.tableAdapterManager.PersonTableAdapter = this.personTableAdapter;
+            this.tableAdapterManager.UpdateOrder = Assignment_5.PersonDataSetTableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete;
+            // 
+            // personBindingNavigator
+            // 
+            this.personBindingNavigator.AddNewItem = this.bindingNavigatorAddNewItem;
+            this.personBindingNavigator.BindingSource = this.personBindingSource;
+            this.personBindingNavigator.CountItem = this.bindingNavigatorCountItem;
+            this.personBindingNavigator.DeleteItem = this.bindingNavigatorDeleteItem;
+            this.personBindingNavigator.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.bindingNavigatorMoveFirstItem,
+            this.bindingNavigatorMovePreviousItem,
+            this.bindingNavigatorSeparator,
+            this.bindingNavigatorPositionItem,
+            this.bindingNavigatorCountItem,
+            this.bindingNavigatorSeparator1,
+            this.bindingNavigatorMoveNextItem,
+            this.bindingNavigatorMoveLastItem,
+            this.bindingNavigatorSeparator2,
+            this.bindingNavigatorAddNewItem,
+            this.bindingNavigatorDeleteItem,
+            this.personBindingNavigatorSaveItem});
+            this.personBindingNavigator.Location = new System.Drawing.Point(0, 0);
+            this.personBindingNavigator.MoveFirstItem = this.bindingNavigatorMoveFirstItem;
+            this.personBindingNavigator.MoveLastItem = this.bindingNavigatorMoveLastItem;
+            this.personBindingNavigator.MoveNextItem = this.bindingNavigatorMoveNextItem;
+            this.personBindingNavigator.MovePreviousItem = this.bindingNavigatorMovePreviousItem;
+            this.personBindingNavigator.Name = "personBindingNavigator";
+            this.personBindingNavigator.PositionItem = this.bindingNavigatorPositionItem;
+            this.personBindingNavigator.Size = new System.Drawing.Size(800, 25);
+            this.personBindingNavigator.TabIndex = 0;
+            this.personBindingNavigator.Text = "bindingNavigator1";
+            // 
+            // personDataGridView
+            // 
+            this.personDataGridView.AutoGenerateColumns = false;
+            this.personDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.personDataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn1,
+            this.dataGridViewTextBoxColumn2,
+            this.dataGridViewTextBoxColumn3});
+            this.personDataGridView.DataSource = this.personBindingSource;
+            this.personDataGridView.Location = new System.Drawing.Point(12, 28);
+            this.personDataGridView.Name = "personDataGridView";
+            this.personDataGridView.Size = new System.Drawing.Size(337, 220);
+            this.personDataGridView.TabIndex = 1;
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            this.dataGridViewTextBoxColumn1.DataPropertyName = "PersonID";
+            this.dataGridViewTextBoxColumn1.HeaderText = "PersonID";
+            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            this.dataGridViewTextBoxColumn1.ReadOnly = true;
+            this.dataGridViewTextBoxColumn1.Visible = false; // Hide PersonID Column
+            // 
+            // dataGridViewTextBoxColumn2
+            // 
+            this.dataGridViewTextBoxColumn2.DataPropertyName = "Name";
+            this.dataGridViewTextBoxColumn2.HeaderText = "Name";
+            this.dataGridViewTextBoxColumn2.Name = "Name";
+            // 
+            // dataGridViewTextBoxColumn3
+            // 
+            this.dataGridViewTextBoxColumn3.DataPropertyName = "Phone";
+            this.dataGridViewTextBoxColumn3.HeaderText = "Phone";
+            this.dataGridViewTextBoxColumn3.Name = "Phone";
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.personDataGridView);
+            this.Controls.Add(this.personBindingNavigator);
+            this.Controls.Add(this.deleteButton);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.phoneTextBox);
+            this.Controls.Add(this.phoneLabel);
+            this.Controls.Add(this.nameTextBox);
+            this.Controls.Add(this.nameLabel);
+            this.Name = "Form1";
+            this.Text = "Person Data";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.personDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.personBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.personBindingNavigator)).EndInit();
+            this.personBindingNavigator.ResumeLayout(false);
+            this.personBindingNavigator.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.personDataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private PersonDataSet personDataSet;
+        private System.Windows.Forms.BindingSource personBindingSource;
+        private PersonDataSetTableAdapters.PersonTableAdapter personTableAdapter;
+        private PersonDataSetTableAdapters.TableAdapterManager tableAdapterManager;
+        private System.Windows.Forms.BindingNavigator personBindingNavigator;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMoveFirstItem;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMovePreviousItem;
+        private System.Windows.Forms.ToolStripSeparator bindingNavigatorSeparator;
+        private System.Windows.Forms.ToolStripTextBox bindingNavigatorPositionItem;
+        private System.Windows.Forms.ToolStripLabel bindingNavigatorCountItem;
+        private System.Windows.Forms.ToolStripSeparator bindingNavigatorSeparator1;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMoveNextItem;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMoveLastItem;
+        private System.Windows.Forms.ToolStripSeparator bindingNavigatorSeparator2;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorAddNewItem;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorDeleteItem;
+        private System.Windows.Forms.ToolStripButton personBindingNavigatorSaveItem;
+        private System.Windows.Forms.DataGridView personDataGridView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn3;
+
+        // Controls for Name, Phone, Save, Add, and Delete buttons
+        private System.Windows.Forms.Label nameLabel;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.Label phoneLabel;
+        private System.Windows.Forms.TextBox phoneTextBox;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.Button deleteButton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Assignment_5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void PersonBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.personBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.personDataSet);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'personDataSet.Person' table. You can move, or remove it, as needed.
+            this.personTableAdapter.Fill(this.personDataSet.Person);
+            this.personBindingSource.DataMember = "Person";
+
+
+            this.bindingNavigatorAddNewItem.Enabled = false;
+            this.bindingNavigatorDeleteItem.Enabled = false;
+
+            nameTextBox.TabIndex = 0;
+            phoneTextBox.TabIndex = 1;  
+            saveButton.TabIndex = 2;
+            addButton.TabIndex = 3;
+            deleteButton.TabIndex = 4;
+
+        }
+        private void DeleteButton_Click(object sender, EventArgs e)
+        {
+            this.personBindingSource.RemoveCurrent();
+            this.personTableAdapter.Update(this.personDataSet.Person);
+        }
+
+        private void AddButton_Click(object sender, EventArgs e)
+        {
+            this.personBindingSource.AddNew();
+        }
+    }
+}
